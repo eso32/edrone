b24f49217332d16265818336e6b919420c4ff1ea
Tomek <tomek.szlapa@gmail.com>
2017-09-27 12:18:24 +0200

    Opening commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name edroneApp
+ * @description
+ * # edroneApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('edroneApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name edroneApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the edroneApp
+ */
+angular.module('edroneApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
